1d7bc83ca924c7ec44d428036d5611edd0d63329
wolfnettech <wolfnettech@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-09 23:21:48 +0000

    tagging version 1.0.7
    git-svn-id: https://plugins.svn.wordpress.org/wolfnet-idx-for-wordpress/tags/1.0.7@610201 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wolfnet.php
+++ wolfnet.php
@@ -5,7 +5,7 @@
 Plugin Name:  WolfNet IDX for WordPress
 Plugin URI:   http://wordpress.wolfnet.com
 Description:  The WolfNet IDX for WordPress plugin provides IDX search solution integration with any WordPress website.
-Version:      1.0.5
+Version:      1.0.7
 Author:       WolfNet Technologies, LLC.
 Author URI:   http://www.wolfnet.com
 
@@ -21,17 +21,17 @@ com_ajmichels_common_autoLoader::getInstance( dirname( __FILE__ ) );
  * @contributors  AJ Michels (http://aj.michels@wolfnet.com)
  * @version       1.0
  * @copyright     Copyright (c) 2012, WolfNet Technologies, LLC
- *                
+ *
  *                This program is free software; you can redistribute it and/or
  *                modify it under the terms of the GNU General Public License
  *                as published by the Free Software Foundation; either version 2
  *                of the License, or (at your option) any later version.
- *                
+ *
  *                This program is distributed in the hope that it will be useful,
  *                but WITHOUT ANY WARRANTY; without even the implied warranty of
  *                MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *                GNU General Public License for more details.
- *                
+ *
  *                You should have received a copy of the GNU General Public License
  *                along with this program; if not, write to the Free Software
  *                Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -58,8 +58,8 @@ implements com_ajmichels_common_iSingleton
 	/* PROPERTIES ******************************************************************************* */
 
 	public $majorVersion = '1.0';
-	public $minorVersion = '5';
-	public $version      = '1.0.5';
+	public $minorVersion = '7';
+	public $version      = '1.0.7';
 
 
 	/* CONSTRUCT PLUGIN ************************************************************************* */
