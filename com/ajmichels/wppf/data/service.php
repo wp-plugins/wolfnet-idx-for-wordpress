3cd671e76a5895a6d68b3d85a1a9a0fe14569dee
wolfnettech <wolfnettech@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-09 13:56:35 +0000

    tagging version 1.0.6
    git-svn-id: https://plugins.svn.wordpress.org/wolfnet-idx-for-wordpress/tags/1.0.6@610045 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- com/ajmichels/wppf/data/service.php
+++ com/ajmichels/wppf/data/service.php
@@ -10,39 +10,39 @@ if ( !class_exists( 'com_ajmichels_wppf_data_service' ) ) {
  * @contributors  AJ Michels (http://ajmichels.com)
  * @version       1.0
  * @copyright     Copyright (c) 2012, AJ Michels
- *                
+ *
  *                Licensed under the Apache License, Version 2.0 (the "License");
  *                you may not use this file except in compliance with the License.
  *                You may obtain a copy of the License at
- *                
+ *
  *                   http://www.apache.org/licenses/LICENSE-2.0
- *                
+ *
  *                Unless required by applicable law or agreed to in writing, software
  *                distributed under the License is distributed on an "AS IS" BASIS,
  *                WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *                See the License for the specific language governing permissions and
  *                limitations under the License.
- * 
+ *
  */
 
 class com_ajmichels_wppf_data_service
 extends com_ajmichels_wppf_abstract_service
 implements com_ajmichels_wppf_interface_iDataService
 {
-	
-	
+
+
 	/* PROPERTIES ******************************************************************************* */
-	
+
 	private $cacheDir;
 	private $cacheTime;
 	private $clearCache = false;
-	
-	
+
+
 	/* SINGLETON ENFORCEMENT ******************************************************************** */
-	
+
 	private static $instance;
-	
-	
+
+
 	public static function getInstance ()
 	{
 		if ( !isset( self::$instance ) ) {
@@ -50,104 +50,100 @@ implements com_ajmichels_wppf_interface_iDataService
 		}
 		return self::$instance;
 	}
-	
-	
+
+
 	/* CONSTRUCTOR ****************************************************************************** */
-	
+
 	private function __construct ()
 	{
 		$this->setCacheDir( $this->formatPath( dirname(__FILE__) . '/cache/' ) );
 		$this->setCacheTime( 15 );
 	}
-	
-	
+
+
 	/* PUBLIC METHODS *************************************************************************** */
-	
+
 	public function getData ( com_ajmichels_wppf_data_webServiceUrl $url )
 	{
 		$cacheSetting = $url->getCacheSetting();
-		
+
+
 		if ( $cacheSetting == 'never' || !$rawData = $this->resolveCachedData( (string) $url ) ) {
-			
-			$old_track = ini_set('track_errors', '1');
-			
-			if ( !@$rawData = file_get_contents( (string) $url, false, $url->getContext() ) ) {
+
+			$this->log( array( 'Getting Fresh Data', (string) $url ) );
+			$httpResponse = wp_remote_get( (string) $url );
+			$this->log( $httpResponse );
+
+			if ( is_wp_error( $httpResponse ) || $httpResponse['response']['code'] != '200' ) {
 				/** TODO: Add support for XML formatted messages */
-				$rawData = '{"error":{"message":"A Connection Error Occured!","status":false'
+				$rawData = '{"error":{"message":"A Connection Error Occured!","status":true'
 				         . ',"detail":"' . (string) $url . '"}}';
 			}
-			
-			ini_set('track_errors', $old_track);
-			
+			else {
+				$rawData = $httpResponse['body'];
+			}
+
 			if ( is_numeric( $cacheSetting ) ) {
 				$this->setCacheTime( $cacheSetting );
 				$this->establishCachedData( (string) $url, $rawData );
 			}
-			
+
 		}
-		
+		else {
+			$this->log( array( 'Getting Cached Data', (string) $url, $this->getCachedFileName( (string) $url ) ) );
+		}
+
 		$data = $this->getDataInterpreter()->parse( $rawData );
-		
-		/*
-		if ( array_key_exists( 'error', $data ) ) {
-			ob_start();
-			var_dump( $data['error'] );
-			$error = ob_get_contents();
-			ob_end_clean();
-			print '<div class="error">' . $error . '</div>';
-			$data = array();
-		}
-		*/
-		
+
 		return $data;
 	}
-	
-	
+
+
 	public function clearCache ()
 	{
 		if ( file_exists( $this->getCacheDir() ) ) {
 			$this->deleteDir( $this->getCacheDir() );
 		}
 	}
-	
-	
+
+
 	/* PRIVATE METHODS ************************************************************************** */
-	
-	
+
+
 	private function resolveCachedData ( $url )
 	{
 		$clearCache  = $this->getClearCache();
-		
+
 		if ( $clearCache ) {
 			$this->clearCache();
 		}
-		
+
 		$cacheFile   = $this->getCacheDir() . $this->getCachedFileName( $url );
 		$fileExists  = file_exists( $cacheFile );
 		$cacheTime   = $this->getCacheTime();
 		$currentTime = time();
 		$expireTime  = $currentTime - ( $cacheTime * 60 );
-		
+
 		if ( $fileExists && $expireTime < filemtime( $cacheFile ) ) {
 			return file_get_contents( $cacheFile );
 		}
 		else {
 			return false;
 		}
-		
+
 	}
-	
-	
+
+
 	private function establishCachedData ( $url, $rawData )
 	{
 		$cacheTime = $this->getCacheTime();
-		
+
 		if ( $cacheTime > 0 ) {
-		
+
 			$cacheFilename = $this->getCacheDir() . $this->getCachedFileName( $url );
-			
+
 			$this->log( $cacheFilename );
-			
+
 			if ( file_exists( $this->getCacheDir() ) ) {
 				if ( file_exists( $cacheFilename ) ) {
 					unlink( $cacheFilename );
@@ -156,16 +152,16 @@ implements com_ajmichels_wppf_interface_iDataService
 			else {
 				mkdir( $this->getCacheDir() );
 			}
-			
+
 			$cacheFile    = fopen(  $cacheFilename, 'x' );
 			$writeResults = fwrite( $cacheFile, $rawData );
 			                fclose( $cacheFile );
-		
+
 		}
-		
+
 	}
-	
-	
+
+
 	private function getCachedFileName ( $url )
 	{
 		$diClass = get_class( $this->getDataInterpreter() );
@@ -173,58 +169,58 @@ implements com_ajmichels_wppf_interface_iDataService
 		$filename = $hash . '.tmp';
 		return $filename;
 	}
-	
-	
+
+
 	/* ACCESSOR METHODS ************************************************************************* */
-	
+
 	private function getCacheDir ()
 	{
 		return $this->cacheDir;
 	}
-	
-	
+
+
 	private function setCacheDir ( $path )
 	{
 		$this->cacheDir = $path;
 	}
-	
+
 	public function getCacheTime ()
 	{
 		return $this->cacheTime;
 	}
-	
-	
+
+
 	public function setCacheTime ( $minutes )
 	{
 		$this->cacheTime = $minutes;
 	}
-	
+
 	public function getClearCache ()
 	{
 		return $this->clearCache;
 	}
-	
-	
+
+
 	public function setClearCache ( $clear )
 	{
 		$this->clearCache = $clear;
 	}
-	
-	
+
+
 	public function getDataInterpreter ()
 	{
-		
+
 		return $this->dataInterpreter;
 	}
-	
-	
+
+
 	public function setDataInterpreter ( com_ajmichels_wppf_interface_iDataInterpreter $di )
 	{
-		
+
 		$this->dataInterpreter = $di;
 	}
-	
-	
+
+
 }
 
 } // end 'if class_exists'
--- com/ajmichels/wppf/json/service.php
+++ com/ajmichels/wppf/json/service.php
@@ -10,35 +10,35 @@ if ( !class_exists( 'com_ajmichels_wppf_json_service' ) ) {
  * @contributors  AJ Michels (http://ajmichels.com)
  * @version       1.0
  * @copyright     Copyright (c) 2012, AJ Michels
- *                
+ *
  *                Licensed under the Apache License, Version 2.0 (the "License");
  *                you may not use this file except in compliance with the License.
  *                You may obtain a copy of the License at
- *                
+ *
  *                   http://www.apache.org/licenses/LICENSE-2.0
- *                
+ *
  *                Unless required by applicable law or agreed to in writing, software
  *                distributed under the License is distributed on an "AS IS" BASIS,
  *                WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *                See the License for the specific language governing permissions and
  *                limitations under the License.
- * 
+ *
  */
 
 class com_ajmichels_wppf_json_service
 extends com_ajmichels_wppf_abstract_service
 implements com_ajmichels_wppf_interface_iDataInterpreter
 {
-	
-	
+
+
 	private static $instance;
-	
-	
+
+
 	private function __construct ()
 	{
 	}
-	
-	
+
+
 	public static function getInstance ()
 	{
 		if ( !isset( self::$instance ) ) {
@@ -46,25 +46,42 @@ implements com_ajmichels_wppf_interface_iDataInterpreter
 		}
 		return self::$instance;
 	}
-	
-	
+
+
 	public function parse ( $data )
 	{
 		$rtnData = array();
-		
+
 		if ( $data != '' ) {
 			$rtnData = json_decode( $data, true );
+			$this->log( $this->getJsonError( json_last_error() ) );
 		}
-		
+
 		if ( !is_array( $rtnData ) ) {
 			$rtnData = array( $rtnData );
 		}
-		
+
 		return $rtnData;
-		
+
 	}
-	
-	
+
+
+	private function getJsonError ( $errorCode )
+	{
+		$constants   = get_defined_constants(true);
+		$json_errors = array();
+
+		foreach ($constants["json"] as $name => $value) {
+			if (!strncmp($name, "JSON_ERROR_", 11)) {
+				$json_errors[$value] = $name;
+			}
+		}
+
+		return $json_errors[$errorCode];
+
+	}
+
+
 }
 
 } // end 'if class_exists'
