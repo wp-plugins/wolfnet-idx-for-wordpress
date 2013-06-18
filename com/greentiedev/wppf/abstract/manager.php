e7254b1274f12b0e0c2e4f4ea40ec3c68c062303
ajmichels <ajmichels@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-18 14:43:27 +0000

    Releasing new version (1.3.13) with minor updates.
    git-svn-id: https://plugins.svn.wordpress.org/wolfnet-idx-for-wordpress/tags/1.3.13@727941 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ com/greentiedev/wppf/abstract/manager.php
@@ -0,0 +1,66 @@
+<?php
+
+if ( !class_exists( 'com_greentiedev_wppf_abstract_manager' ) ) {
+
+/**
+ *
+ * @package       com.greentiedev.wppf.abstract
+ * @title         manager.php
+ * @extends       com_greentiedev_phpCommon_abstractClass
+ * @contributors  AJ Michels (http://greentiedev.com)
+ * @version       1.0
+ * @copyright     Copyright (c) 2012, AJ Michels
+ *
+ *                Licensed under the Apache License, Version 2.0 (the "License");
+ *                you may not use this file except in compliance with the License.
+ *                You may obtain a copy of the License at
+ *
+ *                   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *                Unless required by applicable law or agreed to in writing, software
+ *                distributed under the License is distributed on an "AS IS" BASIS,
+ *                WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *                See the License for the specific language governing permissions and
+ *                limitations under the License.
+ *
+ */
+
+abstract class com_greentiedev_wppf_abstract_manager
+extends com_greentiedev_phpCommon_abstractClass
+{
+
+
+	/* PROPERTIES ******************************************************************************* */
+
+	private   $plugin  = null;
+	protected $managed = array();
+
+
+	/* PUBLIC METHODS *************************************************************************** */
+
+	public function register ( com_greentiedev_wppf_interface_iManaged &$obj )
+	{
+		if ( $this->getPlugin() != null ) {
+			$obj->setPlugin( $this->getPlugin() );
+		}
+		array_push( $this->managed, $obj );
+	}
+
+
+	/* ACCESSOR METHODS ************************************************************************* */
+
+	public function getPlugin ()
+	{
+		return $this->plugin;
+	}
+
+
+	public function setPlugin ( com_greentiedev_wppf_bootstrap &$obj )
+	{
+		$this->plugin = $obj;
+	}
+
+
+}
+
+} // end 'if class_exists'
