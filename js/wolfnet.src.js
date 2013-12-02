90542e10df5b9ad075b910d2df7b1d9721c81b07
ajmichels <ajmichels@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-02 14:52:02 +0000

    committing new release 1.4.1
    git-svn-id: https://plugins.svn.wordpress.org/wolfnet-idx-for-wordpress/tags/1.4.1@813755 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/wolfnet.src.js
@@ -0,0 +1,122 @@
+/**
+ * This script is a general container for JavaScript used by the plugin.
+ *
+ * @title         wolfnet.src.js
+ * @copyright     Copyright (c) 2012, 2013, WolfNet Technologies, LLC
+ *
+ *                This program is free software; you can redistribute it and/or
+ *                modify it under the terms of the GNU General Public License
+ *                as published by the Free Software Foundation; either version 2
+ *                of the License, or (at your option) any later version.
+ *
+ *                This program is distributed in the hope that it will be useful,
+ *                but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *                MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *                GNU General Public License for more details.
+ *
+ *                You should have received a copy of the GNU General Public License
+ *                along with this program; if not, write to the Free Software
+ *                Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+
+var wolfnet = function ()
+{
+}
+
+wolfnet.initMoreInfo = function (  $moreInfoItems )
+{
+
+	var iconClass = 'wolfnet_moreInfoIcon';
+
+	( function ( $ ) {
+
+		$moreInfoItems.hide();
+
+		$moreInfoItems.each( function () {
+
+			var $item     = $( this );
+			var $icon     = $item.siblings( 'span.' + iconClass );
+
+			if ( $icon.length == 0 ) {
+
+				$icon = $( '<span />' );
+				$icon.addClass( iconClass );
+				$item.before( $icon );
+
+			}
+
+		} );
+
+		if ( typeof $.fn.tooltip != 'undefined' ) {
+
+			$( '.' + iconClass ).tooltip( {
+				showURL     : false,
+				//width       : 200,
+				bodyHandler : function () {
+					return $( this ).siblings( '.wolfnet_moreInfo' ).html();
+				}
+			} );
+
+		}
+
+	} )( jQuery );
+
+}
+
+if ( typeof jQuery != 'undefined' ) {
+
+	( function ( $ ) {
+
+		var isPlaceholderSupported = function ()
+		{
+			var test = document.createElement( 'input' );
+			return ( 'placeholder' in test );
+		}
+
+		$( document ).ready( function () {
+
+			wolfnet.initMoreInfo( $( '.wolfnet_moreInfo' ) );
+
+			( function () {
+
+				if ( !isPlaceholderSupported() ) {
+
+					var $placeHolderInputs = $( 'input[placeholder]');
+
+					var inputBlur = function ()
+					{
+						var $this = $( this );
+						var placeholder = $this.attr( 'placeholder' );
+						if ( $this.val().trim() == '' || $this.val().trim() == placeholder ) {
+							$this.val( placeholder );
+							$this.addClass( 'input-placeholder' );
+						}
+					}
+
+					var inputFocus = function ()
+					{
+						var $this = $( this );
+						var placeholder = $this.attr( 'placeholder' );
+						if ( $this.val().trim() == placeholder ) {
+							$this.val('');
+						}
+						$this.removeClass( 'input-placeholder' );
+
+					}
+
+					$placeHolderInputs.blur( inputBlur );
+					$placeHolderInputs.change( inputBlur );
+					$placeHolderInputs.focus( inputFocus );
+					$placeHolderInputs.submit( inputFocus );
+					$placeHolderInputs.trigger( 'blur' );
+
+				}
+
+			} )();
+
+		} );
+
+	} )( jQuery );
+
+}
