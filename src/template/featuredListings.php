c6879b02f258d797c2a107989b3c2b42cbd13b7f
ajmichels <ajmichels@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-02 14:52:19 +0000

    Publishing version 1.7.8
    git-svn-id: https://plugins.svn.wordpress.org/wolfnet-idx-for-wordpress/tags/1.7.8@1191292 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ src/template/featuredListings.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ *
+ * @title         featuredListings.php
+ * @copyright     Copyright (c) 2012, 2013, WolfNet Technologies, LLC
+ *
+ *                This program is free software; you can redistribute it and/or
+ *                modify it under the terms of the GNU General Public License
+ *                as published by the Free Software Foundation; either version 2
+ *                of the License, or (at your option) any later version.
+ *
+ *                This program is distributed in the hope that it will be useful,
+ *                but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *                MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *                GNU General Public License for more details.
+ *
+ *                You should have received a copy of the GNU General Public License
+ *                along with this program; if not, write to the Free Software
+ *                Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+?>
+
+<?php if (trim($title)!='') { ?>
+    <h2><?php echo $title ?></h2>
+<?php } ?>
+
+<div id="<?php echo $instance_id; ?>" class="wolfnet_widget wolfnet_featuredListings">
+    <?php echo (isset($listingsHtml)) ? $listingsHtml : 'No Listings to Display.'; ?>
+</div>
+
+<script type="text/javascript">
+
+    jQuery(function($){
+        $('#<?php echo $instance_id; ?>').wolfnetScrollingItems({
+              autoPlay  : <?php echo ($autoplay) ? 'true' : 'false'; ?>
+            , direction : <?php echo "'" . $direction . "'"; ?>
+            , speed     : <?php echo $speed; ?>
+        });
+    });
+
+</script>
