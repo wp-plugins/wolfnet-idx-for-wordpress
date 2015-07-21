1dfece799d306f032a0a814836e79ee1ca582c6b
ajmichels <ajmichels@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-21 20:38:10 +0000

    Publishing version 1.7.11
    git-svn-id: https://plugins.svn.wordpress.org/wolfnet-idx-for-wordpress/tags/1.7.11@1203441 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ src/template/listing.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ *
+ * @title         listing.php
+ * @copyright     Copyright (c) 2012, 2013, WolfNet Technologies, LLC
+ *
+ *                This program is free software; you can redistribute it and/or
+ *                modify it under the terms of the GNU General Public License
+ *                as published by the Free Software Foundation; either version 2
+ *                of the License, or (at your option) any later version.
+ *
+ *                This program is distributed in the hope that it will be useful,
+ *                but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *                MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *                GNU General Public License for more details.
+ *
+ *                You should have received a copy of the GNU General Public License
+ *                along with this program; if not, write to the Free Software
+ *                Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+?>
+
+<div id="wolfnet_listing_<?php echo $listing['property_id']; ?>" class="wolfnet_listing" itemscope>
+    <a href="<?php echo $listing['property_url']; ?>" rel="follow">
+        <span class="wolfnet_listingImage"><img src="<?php echo $listing['thumbnail_url']; ?>" alt="Property for sale at <?php echo $listing['address']; ?>" /></span>
+        <span class="wolfnet_price" itemprop="price"><?php echo $listing['listing_price']; ?></span>
+        <span class="wolfnet_bed_bath" title="<?php echo $listing['bedsbaths_full']; ?>"><?php echo $listing['bedsbaths']; ?></span>
+        <span title="<?php echo $listing['address']; ?>">
+            <span class="wolfnet_address"><?php echo $listing['display_address']; ?></span>
+            <span class="wolfnet_location" itemprop="locality"><?php echo $listing['location']; ?></span>
+            <span class="wolfnet_full_address" itemprop="street-address" style="display:none;"><?php echo $listing['address']; ?></span>
+        </span>
+        <div class="wolfnet_branding">
+            <?php if (trim($listing['branding']['logo']) !== '') { ?>
+                <span class="wolfnet_brokerLogo<?php echo ($listing['branding']['type']=='idx') ? ' wolfnet_idxLogo' : ''; ?>">
+                    <img src="<?php echo $listing['branding']['logo']; ?>" />
+                </span>
+            <?php } ?>
+            <span class="wolfnet_brandingMessage">
+                <?php if (trim($listing['branding']['courtesy_text']) !== '') { ?>
+                    <span class="wolfnet_brandingCourtesyText"><?php echo $listing['branding']['courtesy_text']; ?></span>
+                <?php } ?>
+                <?php if (trim($listing['branding']['agent_name']) !== '') { ?>
+                    <span class="wolfnet_brandingAgent wolfnet_brandingAgentName"><?php echo $listing['branding']['agent_name']; ?></span>
+                <?php } ?>
+                <?php if (trim($listing['branding']['agent_phone']) !== '') { ?>
+                    <span class="wolfnet_brandingAgent wolfnet_brandingAgentPhone"><?php echo $listing['branding']['agent_phone']; ?></span>
+                <?php } ?>
+                <?php if (trim($listing['branding']['office_name']) !== '') { ?>
+                    <span class="wolfnet_brandingOffice wolfnet_brandingOfficeName"><?php echo $listing['branding']['office_name']; ?></span>
+                <?php } ?>
+                <?php if (trim($listing['branding']['office_phone']) !== '') { ?>
+                    <span class="wolfnet_brandingOffice wolfnet_brandingOfficePhone"><?php echo $listing['branding']['office_phone']; ?></span>
+                <?php } ?>
+                <?php if (trim($listing['branding']['toll_free_phone']) !== '') { ?>
+                    <span class="wolfnet_brandingTollFreePhone"><?php echo $listing['branding']['toll_free_phone']; ?></span>
+                <?php } ?>
+            </span>
+        </div>
+    </a>
+</div>
---
+++ src/template/listingGrid.php
@@ -0,0 +1,91 @@
+<?php
+
+/**
+ *
+ * @title         listingGrid.php
+ * @copyright     Copyright (c) 2012, 2013, WolfNet Technologies, LLC
+ *
+ *                This program is free software; you can redistribute it and/or
+ *                modify it under the terms of the GNU General Public License
+ *                as published by the Free Software Foundation; either version 2
+ *                of the License, or (at your option) any later version.
+ *
+ *                This program is distributed in the hope that it will be useful,
+ *                but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *                MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *                GNU General Public License for more details.
+ *
+ *                You should have received a copy of the GNU General Public License
+ *                along with this program; if not, write to the Free Software
+ *                Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+
+// HACK: Make sure the API key does not get included in the criteria
+unset($wpMeta['key']);
+
+?>
+
+<div id="<?php echo $instance_id; ?>" class="wolfnet_widget <?php echo $class; ?>">
+
+    <?php if (trim($title) != '') { ?>
+        <h2 class="widget-title"><?php echo $title; ?></h2>
+    <?php } ?>
+
+    <?php if (($maptype == 'above' || $maptype == 'map_only') && $mapEnabled) {
+        echo $map;
+    } ?>
+
+    <?php if ($maptype != 'disabled' && $mapEnabled) {
+        echo $hideListingsTools;
+    } ?>
+
+    <div id="<?php echo $collapseListingsId; ?>" >
+
+        <?php echo $toolbarTop; ?>
+
+        <div class="wolfnet_listings">
+            <?php echo (isset($listingsHtml)) ? $listingsHtml : 'No Listings to Display.'; ?>
+        </div>
+
+        <?php echo $toolbarBottom; ?>
+
+    </div>
+
+    <?php if ($maptype == 'below' && $mapEnabled) {
+        echo $map;
+    } ?>
+
+</div>
+
+<div class="wolfnet_clearfix"></div>
+
+<script type="text/javascript">
+
+    jQuery(function($){
+        var instance = <?php echo "'#" . $instance_id . "';"; ?>
+
+        $(instance).wolfnetToolbar({
+             numrows          : <?php echo $wpMeta['maxrows'] . "\n"; ?>
+            ,criteria         : <?php echo json_encode($wpMeta) . "\n"; ?>
+            ,maxResults       : <?php echo $wpMeta['total_rows'] . "\n"; ?>
+            ,itemsPerPageData : <?php echo json_encode($itemsPerPage) . "\n"; ?>
+            ,sortOptionsData  : <?php echo json_encode($sortOptions) . "\n"; ?>
+        });
+        $(instance).filter('.wolfnet_listingGrid').wolfnetListingGrid();
+        $(instance).filter('.wolfnet_propertyList').wolfnetPropertyList();
+
+    });
+
+
+    var maptype = '<?php echo "$maptype"; ?>'
+    if (maptype == 'map_only') {
+
+        var collapseListingsId = '<?php echo $collapseListingsId; ?>';
+        var hideId             = '<?php echo $hideListingsId; ?>';
+        var showId             = '<?php echo $showListingsId; ?>';
+
+        wolfnet.hideListings(collapseListingsId,hideId,showId);
+    }
+
+</script>
